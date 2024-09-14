e3663f328c3c268e083dd666880fe27b99e4457c
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-14 19:19:19 +0545

    updated server file

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import ContactRoutes from  './routes/ContactRoutes.js'
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import ContactRoutes from "./routes/ContactRoutes.js";
 
-import cors from 	'cors'
+import cors from "cors";
 
-//configure env
 dotenv.config();
 
 //databse config
@@ -23,25 +22,24 @@ const app = express();
 //middelwares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //routes
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/category",categoryRoutes);
-app.use('/api/v1/product', productRoutes );
-app.use('/api/v1/contact', ContactRoutes );
-
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/contact", ContactRoutes);
 
 //rest api
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to e-commerce app</h1>")
-  });
-  
-  
+  res.send("<h1>Welcome to e-commerce app</h1>");
+});
 
-  //PORT
-  const PORT = process.env.PORT || 8080;
+//PORT
+const PORT = process.env.PORT || 8080;
 
-  app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.DEV_MODE} mode port ${PORT}`.bgCyan.white);
-  });
+app.listen(PORT, () => {
+  console.log(
+    `Server running on ${process.env.DEV_MODE} mode port ${PORT}`.bgCyan.white
+  );
+});
